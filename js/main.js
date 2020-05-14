b686774b82d5fe35f175f3cb1afe9054c823a01c
mohamadreza karimi <m.karimipost@gmail.com>
2020-05-14 22:10:09 +0430

    set first slide of discont-slider and made it touchable

--- js/main.js
+++ js/main.js
@@ -52,3 +52,22 @@ jssor_slider1_init = function () {
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
             //#endregion responsive code end
         };
+
+        $(document).ready(function() {
+            $(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
+
+});
